da3bce6689b41f3095e39c2c0c82634eb73ff6b1
Devin Crichton <devin.crichton@gmail.com>
2013-07-11 17:06:12 -0400

    More indentation fixes.

--- scripts/block_2.py
+++ scripts/block_2.py
@@ -19,26 +19,23 @@ if __name__ == '__main__':
         stars_data=SegueData(args.data_filepath, columns=['RV_ADOP','DIST_ADOP','FEH_ADOP','L','B'],
 			     memmap = True, nrows = 10000)
 
-        stars_data.cut_bad_data()
+	stars_data.cut_bad_data()
 
-        GCENT, Z = [], []
-        for glong, glat, dist in zip(stars_data.data_dict['L'],
-                                     stars_data.data_dict['B'],
-                                     stars_data.data_dict['DIST_ADOP']):
-            glong = u.deg*glong
-            glat = u.deg*glat
-            dist = u.kiloparsec*dist
-            x,y,z = helio2cartesian(glong,glat,dist)
-            Z.append(z)
-            print(len(Z),len(stars_data.data_dict['L']))
-            GCENT.append((x**2+y**2+z**2)**.5)
+	GCENT, Z = [], []
+	for glong, glat, dist in zip(stars_data.data_dict['L'],
+	                             stars_data.data_dict['B'],
+	                             stars_data.data_dict['DIST_ADOP']):
+		glong = u.deg*glong
+		glat = u.deg*glat
+		dist = u.kiloparsec*dist
+		x,y,z = helio2cartesian(glong,glat,dist)
+		Z.append(z)
+		print(len(Z),len(stars_data.data_dict['L']))
+		GCENT.append((x**2+y**2+z**2)**.5)
 
-        stars_data.data_dict['GCENT_DIST'] = GCENT
-        stars_data.data_dict.pop('L')
-        stars_data.data_dict.pop('B')
-        stars_data.data_dict.pop('DIST_ADOP')
+	stars_data.data_dict['GCENT_DIST'] = GCENT
+	stars_data.data_dict.pop('L')
+	stars_data.data_dict.pop('B')
+	stars_data.data_dict.pop('DIST_ADOP')
 
-        stars_data.histogram_data('block_2.pdf')
-
-
-        
+	stars_data.histogram_data('block_2.pdf')
